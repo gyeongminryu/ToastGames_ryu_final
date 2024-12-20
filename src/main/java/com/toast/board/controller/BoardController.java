fa23e2dd5f77f3bdc4cab9176a4e511230dccf89
Han,BeomHee <bumbeomhan@gmail.com>
2024-12-20 17:05:37 +0900

    마이페이지, 게시판 작성

--- src/main/java/com/toast/board/controller/BoardController.java
+++ src/main/java/com/toast/board/controller/BoardController.java
@@ -1,8 +1,19 @@
 package com.toast.board.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.toast.board.service.BoardService;
 
@@ -15,5 +26,39 @@ public class BoardController {
 	public BoardController(BoardService boardService) {
 		this.boardService = boardService;
 	}
+	
+	// spring.servlet.multipart.location=C:/files 이 경로로 주입. !!! 파일 저장위치 !!!
+    @Value("${spring.servlet.multipart.location}")
+    private String uploadAddr;
 
+	@GetMapping(value = "/board_write.go")
+	public String boardWriteForm() {
+		return "board_write";
+	}
+	
+	@PostMapping(value = "/board_write.do")
+	public String boardWrite(@RequestParam Map<String, Object> params, @RequestParam("file") MultipartFile[] files, HttpSession session, Model model) {
+		// 게시글 작성하면서, 파일도 같이 첨부가 되어야 하는 형식.
+		String id = (String) session.getAttribute("loginId"); // 세션에서 로그인한 id를 가져온다.
+		Map<String, Object> memberInfo = boardService.memberInfo(id); // 필요한 개인 정보들을 담아온다.
+		params.putAll(memberInfo);
+		try {
+			boolean boardWrite = boardService.boardWrite(params, files);
+			if (boardWrite) {
+				model.addAttribute("msg", "게시글 작성 완료");
+			} else {
+				model.addAttribute("msg", "게시글 작성 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "board_list"; // 상세보기로 이동하면 좋을 듯.
+	}
+
+	@ResponseBody
+	@GetMapping(value = "/board_list.ajax")
+	public String boardList() {
+		return "board_list";
+	}
+	
 }
--- src/main/java/com/toast/board/service/BoardService.java
+++ src/main/java/com/toast/board/service/BoardService.java
@@ -1,8 +1,15 @@
 package com.toast.board.service;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.toast.board.dao.BoardDAO;
 
@@ -14,6 +21,46 @@ public class BoardService {
 	
 	public BoardService(BoardDAO boardDAO) {
 		this.boardDAO = boardDAO;
+	}
+	
+	// spring.servlet.multipart.location=C:/files 이 경로로 주입. !!! 파일 저장위치 !!!
+	@Value("${spring.servlet.multipart.location}")
+	private String uploadAddr;
+
+	public Map<String, Object> memberInfo(String id) {
+		return boardDAO.memberInfo(id);
+	}
+
+	public boolean boardWrite(Map<String, Object> params, MultipartFile[] files) throws Exception {
+		
+		String file_key = UUID.randomUUID().toString();
+		params.put("file_key", file_key);
+		int boardIdx = boardDAO.boardWrite(params);
+	    int uploader_idx = (Integer) params.get("appo_empl_idx"); // params에서 uploader_idx 가져오기
+		if (files != null && files.length > 0) { // 파일이 업로드 된다면.
+			for (MultipartFile file : files) {
+				if (!file.isEmpty()) {
+					String ori_filename = file.getOriginalFilename();
+					String file_type = ori_filename.substring(ori_filename.lastIndexOf("."));
+					String new_filename = UUID.randomUUID().toString() + "." + file_type;
+					String file_addr = uploadAddr + "/" + new_filename;
+
+					File dest = new File(file_addr);
+					file.transferTo(dest);
+					
+					 Map<String, Object> fileParams = new HashMap<>();
+					    fileParams.put("file_key", file_key);
+					    fileParams.put("uploader_idx", uploader_idx);
+					    fileParams.put("ori_filename", ori_filename);
+					    fileParams.put("new_filename", new_filename);
+					    fileParams.put("file_type", file_type);
+					    fileParams.put("file_addr", file_addr);
+					
+					boardDAO.saveFile(fileParams);
+				}
+			}
+		}
+		return boardIdx > 0;
 	}
 
 }
